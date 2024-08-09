55b8af7c3df3d76b50ef6196417b309a8583a042
ariefhk <ariefrachmanhakim1001@gmail.com>
2024-08-09 08:49:38 +0700

    feat: fixed hello-world-page

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import "./index.css"
 import { createRoot } from "react-dom/client"
 import { RouterProvider } from "react-router-dom"
 import { ThemeProvider } from "./components/provider/theme-provider"
+import { Toaster } from "./components/ui/toaster"
 import { router } from "./router/index.router.jsx"
 
 createRoot(document.getElementById("root")).render(
   <ThemeProvider defaultTheme="light" storageKey="react-theme">
     <RouterProvider router={router} />
+    <Toaster />
   </ThemeProvider>,
 )
